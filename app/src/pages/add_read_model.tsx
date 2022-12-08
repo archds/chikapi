fab8dcaa9bfd8d4b758dd9953e04373e89fda5e9
archds <dsalekseev11@gmail.com>
2022-12-08 23:43:01 +0700

    Formik example

--- app/src/pages/add_read_model.tsx
+++ app/src/pages/add_read_model.tsx
@@ -1,5 +1,7 @@
+import { ErrorMessage, Field, Form, Formik } from "formik"
+import { FormikErrors, FormikValues } from "formik/dist/types"
 import { useState } from "react"
-import { Box, Button, Form, Heading, Level } from "react-bulma-components"
+import { Box, Button, Columns, Form as BulmaForm, Heading, Level } from "react-bulma-components"
 import { useMutation, useQueryClient } from "react-query"
 import KeyFieldForm from "../components/form/keyfield"
 import AddReadModelForm from "../components/form/rmadd"
@@ -10,6 +12,21 @@ import addReadModel, {
   ReadModelValidationData,
   ReadModelValidationError,
 } from "../services/mutations/add_rm"
+
+type AddReadModelFormValues = {
+  name: string
+  path: string
+  id: string
+}
+
+const validateForm = (values: FormikValues) => {
+  let errors: FormikErrors<FormikValues> = {}
+  console.log(values)
+  if (!values.name) {
+    errors.name = "Name is required!"
+  }
+  return errors
+}
 
 function AddReadModelPage() {
   const queryClient = useQueryClient()
@@ -32,30 +49,78 @@ function AddReadModelPage() {
     },
   })
 
+  const initialValues: AddReadModelFormValues = {
+    name: "",
+    path: "",
+    id: "",
+  }
+
+  const onFormSubmit = () => {
+    console.log("Form submitted")
+  }
+
   return (
-    <Box className="add-read-model-box">
-      <Level>
-        <Level.Side align="left">
-          <Level.Item>
-            <Heading>Add Read Model</Heading>
-          </Level.Item>
-        </Level.Side>
-        <Level.Side align="right">
-          <Level.Item>
-            <Button color="primary" type="submit" onClick={() => mutation.mutate(input)} loading={mutation.isLoading}>
-              Submit
+    <Formik initialValues={initialValues} onSubmit={onFormSubmit} validate={validateForm}>
+      {({ isSubmitting }) => (
+        <Form>
+          <Box className="add-read-model-box">
+            <Level>
+              <Level.Side align="left">
+                <Level.Item>
+                  <Heading>Add Read Model</Heading>
+                </Level.Item>
+              </Level.Side>
+              <Level.Side align="right">
+                <Level.Item>
+                  <Button color="primary" type="submit">
+                    Submit
+                  </Button>
+                </Level.Item>
+              </Level.Side>
+            </Level>
+
+            <Columns>
+              <Columns.Column>
+                <BulmaForm.Field>
+                  <BulmaForm.Label>Name</BulmaForm.Label>
+                  <BulmaForm.Control>
+                    <Field name="name" placeholder="My Read Model" as={BulmaForm.Input} />
+                  </BulmaForm.Control>
+                    <ErrorMessage name="name" component={BulmaForm.Help} />
+                </BulmaForm.Field>
+                <BulmaForm.Field>
+                  <BulmaForm.Label>Path</BulmaForm.Label>
+                  <BulmaForm.Control>
+                    <BulmaForm.Input name="path" placeholder="/route/url" />
+                  </BulmaForm.Control>
+                  <BulmaForm.Help color="danger">
+                    <ErrorMessage name="path" />
+                  </BulmaForm.Help>
+                </BulmaForm.Field>
+              </Columns.Column>
+              <Columns.Column>
+                <BulmaForm.Field>
+                  <BulmaForm.Label>ID</BulmaForm.Label>
+                  <BulmaForm.Control>
+                    <BulmaForm.Input name="id" placeholder="any-string-without-spaces" />
+                  </BulmaForm.Control>
+                  <BulmaForm.Help color="danger">
+                    <ErrorMessage name="id" />
+                  </BulmaForm.Help>
+                </BulmaForm.Field>
+              </Columns.Column>
+            </Columns>
+            <BulmaForm.Field>
+              <BulmaForm.Label>Key Fields</BulmaForm.Label>
+              Something must be here! Something must be here!
+            </BulmaForm.Field>
+            <Button color="link" size="small" type="button" disabled={isSubmitting}>
+              Add
             </Button>
-          </Level.Item>
-        </Level.Side>
-      </Level>
-      <AddReadModelForm
-        errors={inputErrors}
-        onIdChange={(e) => setInput({ ...input, id: e.target.value })}
-        onNameChange={(e) => setInput({ ...input, name: e.target.value })}
-        onPathChange={(e) => setInput({ ...input, path: e.target.value })}
-        onKeyFieldsChange={(kfs) => setInput({ ...input, keyFields: kfs })}
-      />
-    </Box>
+          </Box>
+        </Form>
+      )}
+    </Formik>
   )
 }
 
